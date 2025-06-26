6913e281992c8d1199638be7c8a324bac7e820b7
Kross1de <awesome.hubygu@yandex.ru>
2025-06-26 20:14:14 +0600

    Boot: Add GDT and go to protected mode!

--- boot.asm
+++ boot.asm
@@ -1,3 +1,15 @@
+	format binary as "bin"
+
+	;; MBR
+	MBR_SIZE		= 512
+	MBR_MAGIC		= 0xaa55
+	MBR_ENTRY		= 0x7c00
+
+	org MBR_ENTRY
+
+	;; 16 bit real mode
+	use16
+	
 	;; BIOS functions
 	BIOS_A20		= 0x15
 	A20_CHECK_SUPPORT	= 0x2403
@@ -6,20 +18,15 @@
 	
 	BIOS_VIDEO		= 0x10
 	SET_VIDEO_MODE		= 0x00
-	VGA_256			= 0x13
+	VIDEO_MODE		= 0x03
 	WRITE_CHAR_TTY		= 0x0e
 
-	;; MBR
-	MBR_SIZE		= 512
-	MBR_MAGIC		= 0xaa55
-
-	org 0x7c00
 boot:
 	mov ax, A20_CHECK_SUPPORT
 	int BIOS_A20
-	jb .error
+	jb error
 	cmp ah, 0
-	jnz .error
+	jnz error
 
 	cmp al, 1
 	jz .a20_activated
@@ -27,22 +34,28 @@ boot:
 	;; Activate A20 gate
 	mov ax, A20_ENABLE_GATE
 	int BIOS_A20
-	jb .error
+	jb error
 	cmp ah, 0
-	jnz .error
+	jnz error
 
 	.a20_activated:
-	mov al, VGA_256
+	mov al, VIDEO_MODE
 	mov ah, SET_VIDEO_MODE
 	int BIOS_VIDEO
-	
-	mov si, hello
-	jmp .print_and_halt
 
-	.error:
-	mov si, error
+	cli
+	lgdt [gdt_pointer]	; load the GDT table
+	mov eax, cr0
+	or eax, 0x1
+	mov cr0, eax
 
-	.print_and_halt:
+	jmp CODE_SEG:boot32
+
+error:
+	mov si, error_str
+	jmp print_and_halt
+
+print_and_halt:
 	mov ah, WRITE_CHAR_TTY
 	mov bl, 0
 	.loop:
@@ -55,8 +68,68 @@ boot:
 halt:
 	cli			; clear interrupt flag
 	hlt			; halt execution
-hello:	db "Hello, world!", 0
-error:	db "ERROR", 0
+
+	;; Global Descriptor Table
+
+gdt_start:
+	dq 0x0
+gdt_code:
+	dw 0xFFFF
+	dw 0x0
+	db 0x0
+	db 10011010b
+	db 11001111b
+	db 0x0
+gdt_data:
+	dw 0xFFFF
+	dw 0x0
+	db 0x0
+	db 10010010b
+	db 11001111b
+	db 0x0
+gdt_end:
+gdt_pointer:
+	dw gdt_end - gdt_start
+	dd gdt_start
+	CODE_SEG = gdt_code - gdt_start
+	DATA_SEG = gdt_data - gdt_start
+
+	;; 32 bit protected mode
+	use32
+
+	VGA_BUFFER = 0xb8000
+
+boot32:
+	;; WE IN PROTECTED MODE HELLL YEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA...
+
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+
+	mov esi, hello_str
+	jmp print_and_halt32
+
+print_and_halt32:
+	mov ebx, VGA_BUFFER
+	mov ah, 0
+	.loop:
+	lodsb
+	or al, al
+	jz halt
+	or eax, 0x0100
+	mov word [ebx], ax
+	inc ah
+	add ebx, 2
+	jmp .loop
+halt32:
+	cli
+	hlt
+
+hello_str:	db "PROTECTED MODE!", 0
+error_str:	db "ERROR", 0
 
 	times (MBR_SIZE - 2) - ($-$$) db 0
 	dw MBR_MAGIC
