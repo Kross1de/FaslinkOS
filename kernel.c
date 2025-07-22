a040a9061669704dafd680c0848196c4c7a6f63c
Kross1de <awesome.hubygu@yandex.ru>
2025-07-22 13:51:13 +0600

    Kernel: Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,14 +1,14 @@
-#include "include/vga.h"
-#include "include/string.h"
+#include "include/stdio.h"
+#include "include/stdlib.h"
 
 void kmain() {
-    vga_puts("Hello from kernel.c\n");
+    puts("Hello from kernel.c\n");
     char itoa_buf[33];
     char *str = itoa(0x1337BABE, itoa_buf, 16);
     if (str == 0) {
-        vga_puts("itoa failed\n");
+        puts("itoa failed\n");
     } else {
-        vga_puts(str);
-        vga_putnl();
+        puts(str);
+        putnl();
     }
 }
